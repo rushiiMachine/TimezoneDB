9443b4ea1e869bb7b29398df4f9cd69219e0bda3
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-07-30 17:42:09 -0700

    feat: setup react dev proxy

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     paths: function (paths, env) {
+        paths.proxySetup = path.resolve(__dirname, 'setupDevProxy.js');
         paths.appIndexJs = path.resolve(__dirname, 'src-ui/index.tsx');
         paths.appSrc = path.resolve(__dirname, 'src-ui');
         return paths;
