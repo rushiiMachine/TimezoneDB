a3a0ba54ee49a5a380b95e116f41e841cc996c8e
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-07-31 17:37:21 -0700

    fix: assets/colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,15 @@ const darken = (clr, val) => Color(clr).darken(val).rgb().string()
 module.exports = {
     content: ["./src-ui/**/*.{html,tsx}"],
     theme: {
-        extend: {
-            colors: {
-                // "gray": "#95A5A6",
-                // "dark-gray": "#979C9F",
-                // "darker-gray": "#7F8C8D",
-                // "light-gray": "#BCC0C0",
-
-                "blurple": "#5865F2",
-                "blurple-lighter": lighten("#5865F2", 0.1),
-                "grayple": "#99AAB5",
-                "dark-black": "#2C2F33", // DARK_BUT_NOT_BLACK
-                "not-black": "#23272A", // NOT_QUITE_BLACK
-                "red": "#ED4245",
-                "green": "#57F287",
-                "black": "#23272A",
-            },
+        colors: {
+            "blurple": "#5865F2",
+            "blurple-lighter": lighten("#5865F2", 0.1),
+            "dark-black": "#2C2F33", // DARK_BUT_NOT_BLACK
+            "not-black": "#23272A", // NOT_QUITE_BLACK
+            "white": "#FFFFFF",
+            "red": "#ED4245",
+            "green": "#57F287",
+            "black": "#23272A",
         },
     },
     plugins: [],
