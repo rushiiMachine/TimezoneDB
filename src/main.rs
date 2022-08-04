d27add590c0fe10d94690cfcdb6ed655ee83b1f9
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-08-04 14:42:56 -0700

    feat: disable release logging

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ fn rocket() -> Rocket<Build> {
     let figment = Config::figment()
         .merge(("port", *constants::PORT))
         .merge(("databases.main.url", &*constants::POSTGRES_URL))
+        .merge(("ident", false))
         .merge(("address", "0.0.0.0"));
+
+    #[cfg(not(debug_assertions))]
+        let figment = figment.merge(("log_level", "off"));
 
     rocket::custom(figment)
         .attach(database::setup())
