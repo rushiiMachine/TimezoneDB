9118f1975a8dc93845ab312c4dcc43ca76af5cd8
yellowsink <yellowsink@riseup.net>
2023-02-01 17:00:52 +0000

    Fix CORS preflights failing

--- src/utils/cors.rs
+++ src/utils/cors.rs
@@ -1,6 +1,6 @@
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
+use rocket::http::{Header, Method, Status};
 
 pub struct CORS();
 
@@ -13,7 +13,13 @@ impl Fairing for CORS {
         }
     }
 
-    async fn on_response<'r>(&self, _: &'r Request<'_>, response: &mut Response<'r>) {
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        if request.method() == Method::Options {
+            // fix OPTIONS preflights returning 404
+            // and preventing calls from working
+            response.set_status(Status::NoContent);
+        }
+
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
         response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
